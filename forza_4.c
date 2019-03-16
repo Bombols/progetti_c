0df00e7d7ce2bda2635bf8ee62a715a43e98bffa
Bombols <ale@DESKTOP-99F2J2U.localdomain>
2019-03-16 11:42:07 +0100

    funziona,quasi

--- forza_4.c
+++ forza_4.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
-const int r = 6;
-const int c = 6;
+const int r=6;
+const int c=6;
 int Inserimento (char array[r][c], int c, char p){
-    int i=5;
+    int i=r-1;
     while(i>=0){
         if(array[i][c]=='l'){
             array[i][c]=p;
@@ -31,8 +31,7 @@ int Colonna(char array[r][c], int rig, int col, char g){
     for(int i=rig; i>=0; i--){
         if(array[i][col]!=g)
             break;
-        else
-        {
+        else{
             nc+=1;
         }
     }
@@ -47,8 +46,7 @@ int Riga(char array[r][c], int rig, int col, char g){
     for(int i=col; i<r; i++){
         if(array[rig][i]!=g)
             break;
-        else
-        {
+        else{
             nr+=1;
         }
     }
@@ -70,115 +68,111 @@ int Diagonale(char array[r][c], int rig, int col, char g){
     int nd2=0;
     int nr=rig;
     int nc=col;
-    while (nr<r && nc<c) {
-        
+    while (nr<r && nc<c){
         if(array[nr][nc]!=g)
             break;
-        else {
-            nr+=1;
-            nc+=1;
+        else{ 
             nd1+=1;
         }
+        nr+=1;
+        nc+=1;
     }
     nr=rig;
     nc=col;
-    while (nr>=0 && nc>=0) {
-       
-        if(array[nr][nc]!=g)
+    while (nr>=0 && nc>=0){
+       if(array[nr][nc]!=g)
             break;
-        else {
-            nr-=1;
-            nc-=1;
+        else{
             nd1+=1;
         }
+        nr-=1;
+        nc-=1;
     }
     nd1--;
     nr=rig;
     nc=col;
     while (nr>=0 && nc<c) {
-       
-        if(array[nr][nc]!=g)
+       if(array[nr][nc]!=g)
             break;
-        else {
-            nr-=1;
-            nc+=1;
+        else{
             nd2+=1;
         }
+        nr-=1;
+        nc+=1;
     }
     nr=rig;
     nc=col;
     while (nr<c && nc>=0) {
-       
-        if(array[nr][nc]!=g)
+       if(array[nr][nc]!=g)
             break;
-        else {
-            nr+=1;
-            nc-=1;
+        else{
             nd2+=1;
         }
+        nr+=1;
+        nc-=1;
     }
     nd2--;
-    if (nd1<4 || nd2<4)
+    if (nd1<4 && nd2<4)
         return 1;
     return 0;
 }
 
 int main(int argc, char** argv){
-    char forza [r][c];
+    char forza[r][c];
     int cont;
     for(int i=0; i<r; i++){
         for (int j=0; j<c; j++){
-            forza [i][j]='l';
+            forza[i][j]='l';
         }
     }
     stampa(forza);
     printf("Giocatore 1 Rosso, Giocatore 2 Verde\n");
     for(;;){
-        printf("Giocatore 1 inserisci numero di colonna compresa fra 0 e 5:\n");
+        printf("Giocatore 1 inserisci numero di colonna compresa fra 1 e 6:\n");
         int n;
         int max=scanf("%d", &n);
-        if(n>=7 || n<0){
-            printf("Numero colonna non valida!\n");
+        while(n>=7 || n<0){
+            printf("Numero colonna non valida, inserire un numero di colonna compreso tra 1 e 6\n");
+            max=scanf("%d", &n);
         }
-        else {
-            int riga=Inserimento(forza, n, 'R');
-            if(!Colonna(forza, riga, n, 'R')){
-                printf("Giocatore 1 hai vinto!\n");
-                return 0;
-            }
-            if(!Riga(forza, riga, n, 'R')){
-                stampa(forza);
-                printf("Giocatore 1 hai vinto!\n");
-                return 0;
-            }
-            if(!Diagonale(forza, riga, n, 'R')){
-                stampa(forza);
-                printf("Giocatore 1 hai vinto!\n");
-                return 0;
-            }
+        n--;
+        int riga=Inserimento(forza, n, 'R');
+        if(!Colonna(forza, riga, n, 'R')){
+            printf("Giocatore 1 hai vinto!\n");
+             return 0;
+        }
+        if(!Riga(forza, riga, n, 'R')){
+            stampa(forza);
+            printf("Giocatore 1 hai vinto!\n");
+            return 0;
+        }
+        if(!Diagonale(forza, riga, n, 'R')){
+            stampa(forza);
+            printf("Giocatore 1 hai vinto!\n");
+            return 0;
         }
         stampa(forza);
-        printf("Giocatore 2 inserisci numero di colonna compresa fra 0 e 5:\n");
+        printf("Giocatore 2 inserisci numero di colonna compresa fra 1 e 6:\n");
         max=scanf("%d", &n);
-        if(n>=7 || n<0){
-          printf("Numero colonna non valida!\n");
+        while(n>=7 || n<0){
+            printf("Numero colonna non valida, inserire un numero di colonna compreso tra 1 e 6\n");
+            max=scanf("%d", &n);
         }
-        else {
-            int riga=Inserimento(forza, n, 'V');
-            if(!Colonna(forza, riga, n, 'V')){
-                printf("Giocatore 1 hai vinto!");
-                return 0;
-            }
-            if(!Riga(forza, riga, n, 'V')){
-                stampa(forza);
-                printf("Giocatore 2 hai vinto!\n");
-                return 0;
-            }
-            if(!Diagonale(forza, riga, n, 'V')){
-                stampa(forza);
-                printf("Giocatore 2 hai vinto!\n");
-                return 0;
-            }
+        n-=1;
+        riga=Inserimento(forza, n, 'V');
+        if(!Colonna(forza, riga, n, 'V')){
+            printf("Giocatore 1 hai vinto!");
+            return 0;
+        }
+        if(!Riga(forza, riga, n, 'V')){
+            stampa(forza);
+            printf("Giocatore 2 hai vinto!\n");
+            return 0;
+        }
+        if(!Diagonale(forza, riga, n, 'V')){
+            stampa(forza);
+            printf("Giocatore 2 hai vinto!\n");
+            return 0;
         }
         stampa(forza);
     }
